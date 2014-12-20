35ecf3557a0f3aa98a3eb5cb10bc0f026747dd8f
acahyawan <agung.cahyawan@it.unud.ac.id>
2014-12-20 22:12:24 +0800

    commit lagi

--- src/com/example/testtoast/MainActivity.java
+++ src/com/example/testtoast/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(MainActivity.this, "test123", Toast.LENGTH_LONG).show();
+				Toast.makeText(MainActivity.this, "test1234", Toast.LENGTH_LONG).show();
 				
 			}
         	
